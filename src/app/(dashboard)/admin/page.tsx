b6295edbe1aa77549691bb83ab161f077cc5f5dc
Harshendra Subba Reddy Nallamilli <harshendra.nallamilli@darwinbox.io>
2025-02-23 16:11:34 +0530

    Updated Admin Page Front-End

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -2,6 +2,8 @@ import CountChart from "@/components/CountChart";
 import UserCard from "../../../components/UserCard";
 import AttendanceChart from "../../../components/AttendanceChart";
 import FinanceChart from "@/components/FinanceChart";
+import EventCalendar from "@/components/EventCalendar";
+import Announcements from "@/components/Announcements";
 const AdminPage = () => {
   return (
     <div className="p-4 flex gap-4 flex-col md:flex-row">
@@ -31,7 +33,10 @@ const AdminPage = () => {
         </div>
       </div>
       {/* Right */}
-      <div className="w-full lg:w-1/3">Right</div>
+      <div className="w-full lg:w-1/3 flex flex-col gap-8">
+        <EventCalendar />
+        <Announcements />
+      </div>
     </div>
   );
 };
