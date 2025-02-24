72a93c89a7e287f62b2f4f8f359d1a90f43ac2f1
Harshendra Subba Reddy Nallamilli <harshendra.nallamilli@darwinbox.io>
2025-02-24 15:22:23 +0530

    Adjust UserCard component styles; reduce font size and modify gap spacing for improved layout consistency

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 const UserCard = ({ type }: { type: string }) => {
   return (
     <div className="rounded-2xl odd:bg-campDarwinCobaltBlue even:bg-campDarwinSignalBlue p-4 flex-1">
-      <div className="flex justify-between items-center">
-        <span className="text-[12px] bg-white px-2 py-1 rounded-full text-green-800">
+      <div className="flex justify-between items-center gap-1">
+        <span className="text-[10px] bg-white px-2 py-1 rounded-full text-green-800">
           2024/25
         </span>
         <Image src="/more.png" alt="" width={20} height={20} />
