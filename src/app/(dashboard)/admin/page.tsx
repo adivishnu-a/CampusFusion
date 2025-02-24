5c5a9374030e8de01d2636c341ebf37973713936
Adi Vishnu Avula <95145136+adivishnu-a@users.noreply.github.com>
2025-02-24 15:14:59 +0530

    Reduce gap spacing in admin and student page layouts for improved consistency

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -8,7 +8,7 @@ const AdminPage = () => {
   return (
     <div className="p-4 flex gap-4 flex-col md:flex-row">
       {/* Left */}
-      <div className="w-full lg:w-2/3 flex flex-col gap-8">
+      <div className="w-full lg:w-2/3 flex flex-col gap-4">
         {/* User Cards */}
         <div className="flex gap-4 justify-between flex-wrap">
           <UserCard type="student"></UserCard>
@@ -33,7 +33,7 @@ const AdminPage = () => {
         </div>
       </div>
       {/* Right */}
-      <div className="w-full lg:w-1/3 flex flex-col gap-8">
+      <div className="w-full lg:w-1/3 flex flex-col gap-4">
         <EventCalendar />
         <Announcements />
       </div>
