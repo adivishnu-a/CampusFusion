0f05cf1d9934cec13c1584752da32fd8df5fa4f2
Harshendra Subba Reddy Nallamilli <harshendra.nallamilli@darwinbox.io>
2025-02-24 21:56:39 +0530

    Refactor layout structure and styles for Dashboard, Parent, and Teacher pages; add Teachers List page

---
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -0,0 +1,7 @@
+const TeachersListPage = () => {
+  return (
+    <div className=''>TeachersListPage</div>
+  )
+}
+
+export default TeachersListPage
--- src/app/(dashboard)/teacher/page.tsx
+++ src/app/(dashboard)/teacher/page.tsx
@@ -3,7 +3,7 @@ import Announcements from "@/components/Announcements";
 import BigCalendar from '@/components/BigCalendar';
 const TeacherPage = () => {
   return (
-    <div className='flex-1 p-4 flex gap-4 flex-col xl:flex-row'>
+    <div className='p-4 flex gap-4 flex-col xl:flex-row'>
       {/* Left */}
       <div className='w-full xl:w-2/3'>
         <div className='h-full bg-white p-4 rounded-xl shadow-sm'>
@@ -13,7 +13,7 @@ const TeacherPage = () => {
       </div>
       {/* Right */}
       <div className="w-full lg:w-1/3 flex flex-col gap-4">
-        {/* <EventCalendar /> */}
+        <EventCalendar />
         <Announcements />
       </div>
     </div>
