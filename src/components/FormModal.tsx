77c7fedca4c13e6269461338f27b3418aab56672
Adi Vishnu Avula <95145136+adivishnu-a@users.noreply.github.com>
2025-02-26 18:35:54 +0530

    Update delete button color in FormModal for improved visibility

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -60,7 +60,7 @@ const FormModal = ({
         <span className="text-center font-medium">
           All data will be lost. Are you sure you want to delete this {table}?
         </span>
-        <button className="bg-red-700 text-white py-2 px-4 rounded-md border-none w-max self-center">
+        <button className="bg-campDarwinCandyPeach text-white py-2 px-4 rounded-md border-none w-max self-center">
           Delete
         </button>
       </form>
